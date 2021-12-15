9fd5fe47ae3ce9375b166f9ba5f3e4539f99e099
Leandro Antunez <leandro_antunez@outlook.com>
2021-12-15 17:21:59 -0300

    Bootstrap added.

--- src/tareas-laravel/routes/web.php
+++ src/tareas-laravel/routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/saludos', function () {
+Route::get('/todos', function () {
     return view('app');
 });
