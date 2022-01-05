d6a4f1dd28a4cbee9753b097af2d81b3757a3b7d
Leandro Antunez <leandro_antunez@outlook.com>
2022-01-05 12:11:20 -0300

    To-do's now can be updated.

--- src/tareas-laravel/app/Http/Controllers/TodosController.php
+++ src/tareas-laravel/app/Http/Controllers/TodosController.php
@@ -19,6 +19,28 @@ class TodosController extends Controller
         return redirect()->route('todos')->with('success', 'Tarea creada correctamente');
     }
 
+    public function index() {
+        $todos = Todo::all();
+        return view('todos.index', ['todos' => $todos]);
+    }
+
+    public function show($id) {
+        $todo = Todo::find($id);
+        return view('todos.show', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id) {
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->save();
+        return redirect()->route('todos')->with('success', 'Tarea actualizada.');
+    }
+
+    public function destroy() {
+        $todos = Todo::all();
+        return view('todos.index', ['todos' => $todos]);
+    }
+
     // store
     // update
     // destroy
